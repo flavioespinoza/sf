dc84d0c42b810ed9bae7559f058a4c8f00743126
flavioespinoza <flavio.espinoza@gmail.com>
2018-06-22 12:12:50 -0600

    first commit

---
+++ flightplan.js
@@ -0,0 +1,49 @@
+var plan = require('flightplan');
+var log = require('ololog').configure({locate: false})
+
+var appName = 'sf';
+var username = 'deploy';
+var startFile = 'bin/www';
+
+
+var tmpDir = appName+'-' + new Date().getTime();
+
+var agent = process.env.SSH_AUTH_SOCK
+log.cyan(agent)
+
+
+
+// configuration
+plan.target('production', [{
+    host: '174.138.55.16',
+    username: 'root',
+    agent: agent
+}]);
+
+// run commands on localhost
+plan.local(function(local) {
+  // uncomment these if you need to run a build on your machine first
+  // local.log('Run build');
+  // local.exec('gulp build');
+
+  local.log('Copy files to remote hosts');
+
+  var filesToCopy = local.exec('git ls-files', {silent: true});
+  // rsync files to all the destination's hosts
+  local.transfer(filesToCopy, '/tmp/' + tmpDir);
+});
+
+// run commands on remote hosts (destinations)
+plan.remote(function(remote) {
+  remote.log('Move folder to root');
+  remote.sudo('cp -R /tmp/' + tmpDir + ' ~', {user: username});
+  remote.rm('-rf /tmp/' + tmpDir);
+
+  remote.log('Install dependencies');
+  remote.sudo('npm --production --prefix ~/' + tmpDir + ' install ~/' + tmpDir, {user: username});
+
+  remote.log('Reload application');
+  remote.sudo('ln -snf ~/' + tmpDir + ' ~/'+appName, {user: username});
+  remote.exec('forever stop ~/'+appName+'/'+startFile, {failsafe: true});
+  remote.exec('forever start ~/'+appName+'/'+startFile);
+});
